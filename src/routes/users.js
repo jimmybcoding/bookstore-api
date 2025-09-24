d6b2bfd08a7c356697c3875c3e0e08c3c0cbd526
James Barbour <jameskbarbour1@gmail.com>
2025-09-24 17:06:38 -0400

    Add first route

---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+import prisma from '../db.js';
+
+export async function getUsers(req, res) {
+    try {
+        const users = await prisma.user.findMany()
+        res.json(users);
+        console.log(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch users' });
+    };
+};
